a6319e296bf231e75d9ad01e13e8276fb1542e50
Michael Oroyo <63488970+Michle99@users.noreply.github.com>
2021-02-04 11:03:46 -0500

    Add files via upload

---
+++ test.js
@@ -0,0 +1,15 @@
+let message;
+message = 'Hello';
+console.log(message);
+
+
+let admin = 'John';
+let name;
+name = admin;
+console.log(admin);
+console.log('Michael');
+
+
+let nameOfOurPlanet = 'Earth';
+let visitor = 'currentUser';
+console.log(visitor);
