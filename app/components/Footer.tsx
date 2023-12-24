dfd712959fb7ef1ce0b097a5edee1c3bea03ab62
Michle99 <sammiyonyx@gmail.com>
2023-12-24 15:42:20 -0500

    updated `Footer.tsx`

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,21 +1,36 @@
+import { SocialIcon } from "react-social-icons";
+
 export default function Footer() {
     return (
-      <footer className="border-t border-zinc-800 mt-24">
-        <div className="max-w-7xl mx-auto flex lg:flex-row flex-col items-center lg:justify-between justify-center gap-y-4 md:px-16 px-6 py-8 text-zinc-400">
-          <small className="text-center duration-200 font-mono">
-            All rights reserved &copy; {new Date().getFullYear()}
-          </small>
-  
-          <small className="hover:text-white duration-200">
-            <a
-              href="https://github.com/Michle99/"
-              target="_blank"
-              rel="noreferrer noopener"
-            >
-            <span className="text-green-400">Michle99</span>
-            </a>
-          </small>
-        </div>
-      </footer>
+        <footer className="border-t border-zinc-800 mt-8 md:mt-16">
+          <div className="max-w-7xl mx-auto flex flex-col md:flex-row items-center justify-center md:justify-between md:px-16 px-6 py-8 text-zinc-400">
+            <div className="flex items-center gap-x-4 md:order-2">
+             <SocialIcon
+                url="https://github.com/Michle99/"
+                target="_blank"
+                rel="noreferrer noopener"
+                style={{ height: 30, width: 30 }}
+             />
+             <SocialIcon
+                url="https://www.linkedin.com/in/m0ry"
+                target="_blank"
+                rel="noreferrer noopener"
+                style={{ height: 30, width: 30 }}
+             />
+             <SocialIcon
+                url="https://twitter.com/Yehana01"
+                target="_blank"
+                rel="noreferrer noopener"
+                style={{ height: 30, width: 30 }}
+             />
+            </div>
+
+            <div className="flex flex-col items-center md:flex-row md:order-1 md:mt-0 mt-4 md:text-right">
+              <small className="text-center duration-200 font-mono">
+               All rights reserved &copy; {new Date().getFullYear()}
+             </small>
+            </div>
+          </div>
+        </footer>
     );
   }
