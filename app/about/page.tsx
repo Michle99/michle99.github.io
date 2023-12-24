b6aaa3594a99289dc02aa70ec96b63e86d1eab94
Michle99 <sammiyonyx@gmail.com>
2023-12-24 14:58:08 -0500

    Added `about/page.tsx`

---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+// All nextjs pages have to "export default" @14:57 12/24/23
+const About = () => {
+    return (
+        <div className="about text-center">
+            <h1>This is the about section.</h1>
+        </div>
+    )
+}
+
+export default About;
